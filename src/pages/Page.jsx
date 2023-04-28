fcf78205e24b3ca94cf70c73f52595afe63e96cf
PDimitroww <plamen.vn.96@gmail.com>
2023-04-28 15:29:49 +0300

    added Paginations and some styles

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,12 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+import Pagination from "../components/pagination/Pagination";
 import axios from "axios";
 
 // components
 import { GameBox } from "../components/GameBox";
 const key = import.meta.env.VITE_API_KEY;
+const pageSize = 6;
 
 export const Page = () => {
   const [games, setGames] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const gamesInfo = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * pageSize;
+    const lastPageIndex = firstPageIndex + pageSize;
+    return games.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
 
   useEffect(() => {
     const handleGameList = () => {
@@ -31,16 +41,23 @@ export const Page = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <div className="container">
         <button className="btn viewAll__Btn">View all</button>
         <div className="items__grid">
-          {games.map((game, index) => {
-            console.log(game);
+          {gamesInfo.map((game, index) => {
             return <GameBox key={index} game={game} />;
           })}
         </div>
+
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={games.length}
+          pageSize={pageSize}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
       </div>
-    </div>
+    </>
   );
 };
